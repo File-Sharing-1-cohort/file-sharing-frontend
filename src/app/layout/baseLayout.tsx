3e19fde7d69031da988fab7f98f1424810b8a322
StanislavRadas <stasradysh@gmail.com>
2024-10-09 15:06:07 +0200

    fix--dsn+add--gitignore

--- src/app/layout/baseLayout.tsx
+++ src/app/layout/baseLayout.tsx
@@ -1,11 +1,13 @@
 import { Footer } from '@/widgets/footer';
 import { Header } from '@/widgets/header';
-import * as Sentry from "@sentry/react";
-
+import * as Sentry from '@sentry/react';
+import dotenv from 'dotenv';
 import { Layout } from '@/shared/ui';
 
+dotenv.config();
+
 Sentry.init({
-  dsn: "https://3359b7dc2d57d7688c4c99a593949a48@o4508087975608320.ingest.de.sentry.io/4508087980654672",
+  dsn: process.env.SENTRY_DSN,
   integrations: [
     Sentry.browserTracingIntegration(),
     Sentry.replayIntegration(),
@@ -13,7 +15,7 @@ Sentry.init({
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
   // Session Replay
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
