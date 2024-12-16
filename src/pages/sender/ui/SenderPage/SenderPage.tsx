fba0316bd833fb566359b9af31294a8a1d0f21be
StanislavRadas <stasradysh@gmail.com>
2024-12-16 16:28:55 +0100

    fix time toast

--- src/pages/sender/ui/SenderPage/SenderPage.tsx
+++ src/pages/sender/ui/SenderPage/SenderPage.tsx
@@ -139,7 +139,7 @@ const SenderPage: React.FC = () => {
             </div>
           ),
           {
-            duration: 3000,
+            duration: Infinity,
             position: 'top-center',
             className: `
                 fixed left-1/2 transform -translate-x-1/2
@@ -179,7 +179,7 @@ const SenderPage: React.FC = () => {
           </div>
         ),
         {
-          duration: 3000,
+          duration: Infinity,
           position: 'top-center',
           className: `
                 fixed left-1/2 transform -translate-x-1/2
@@ -231,7 +231,7 @@ const SenderPage: React.FC = () => {
           </div>
         ),
         {
-          duration: 3000,
+          duration: Infinity,
           position: 'top-center',
           className: `
                 fixed left-1/2 transform -translate-x-1/2
@@ -330,7 +330,7 @@ const SenderPage: React.FC = () => {
           </div>
         ),
         {
-          duration: 3000,
+          duration: Infinity,
           position: 'top-center',
           className: `
                 fixed left-1/2 transform -translate-x-1/2
