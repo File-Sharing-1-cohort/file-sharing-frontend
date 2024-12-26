5f52d5dff6b92b1b1bc7fd3d6a5fd3b2a9d487f9
Oleksii Hnatiienko <ohnatiienko@auvik.com>
2024-12-26 16:26:48 +0200

    div-fix

--- src/pages/recipient/ui/DownloadFile/DownloadFile.tsx
+++ src/pages/recipient/ui/DownloadFile/DownloadFile.tsx
@@ -385,12 +385,13 @@ const DownloadFile = () => {
               </button>
             </div>
           </div>
-        )}
-        <div className="flex justify-center items-center gap-10">
-          <img src={download} alt="Download" />
-          <img src={compress} alt="Compress" />
-          <img src={share} alt="Share" />
         </div>
+      )}
+      <div className="flex justify-center items-center gap-10">
+        <img src={download} alt="Download" />
+        <img src={compress} alt="Compress" />
+        <img src={share} alt="Share" />
+      </div>
     </section>
   );
 };
