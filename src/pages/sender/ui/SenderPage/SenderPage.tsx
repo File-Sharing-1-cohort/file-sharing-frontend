65598bb20386d08849641aa4e556926c63fccaa1
Oleksii Hnatiienko <ohnatiienko@auvik.com>
2024-12-30 19:32:30 +0200

    data-testid="selected-file-name"

--- src/pages/sender/ui/SenderPage/SenderPage.tsx
+++ src/pages/sender/ui/SenderPage/SenderPage.tsx
@@ -314,7 +314,10 @@ const SenderPage: React.FC = () => {
                 <ul>
                   {selectedFiles.map(file => (
                     <li key={file.name}>
-                      <div className="flex justify-between py-20">
+                      <div
+                        data-testid="selected-file-name"
+                        className="flex justify-between py-20"
+                      >
                         <p className="text-[20px] ">{file.name}</p>
                         <p className="text-[20px]">
                           {formatFileSize(file.size)}
